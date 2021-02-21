007cc5f26b23e6283b9cffff403391faf427530d
ken025 <kennethmanuel250@gmail.com>
2021-02-21 17:03:31 -0500

    added products to the admin area

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+# Customize
+class ProductAdmin(admin.ModelAdmin):
+    # specify what columns will be visible (model's attributes)
+    list_display = ('name', 'price', 'stock')
+# tell django we want to manage our products in the admin area
+admin.site.register(Product, ProductAdmin)
+# generates form
