f348212a5a1b8ebf2dec005622d0d8511090fa3f
ken025 <kennethmanuel250@gmail.com>
2021-02-27 21:34:24 -0500

    Added urls file and profile model

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Profile
+
+
+def my_profile(request):
+	my_user_profile = Profile.objects.filter(user=request.user).first()
+
+
+	return render(request, "profile.html")
